d3ab6ddcdc25740e0fc6128df3ffa2d55aab0e12
fabienridolfo <fabienridolfo@10.4.95.113>
2016-03-08 14:06:04 +0100

    1er test

---
+++ esiea.org/src/main/java/fabienr/esiea/org/App.java
@@ -0,0 +1,13 @@
+package fabienr.esiea.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
